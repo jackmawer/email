6dde3e2bf283c0f2628c1c86bf333bad9e3a7dd7
Thomas Haywood <32278068+LordHaywood@users.noreply.github.com>
2024-07-29 19:31:51 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,44 @@
 async function handleEmail(message, env, ctx) {
 	const configMeta = await env.config.list();
-	const domains = configMeta.keys.map(key => key.name);
-	
-	// Email comes in - get the domain part of the email.
-	const incomingDomain = message.to.split("@")[1];
+	const incomingDomain = (message.to || "").split("@")[1];
 
 	// Check if the domain is in the config.
+	const domains = (Array.isArray(configMeta.keys) ? configMeta.keys : []).map(key => key.name);
 	if (domains.includes(incomingDomain)) {
 		const domainConfig = JSON.parse(await env.config.get(incomingDomain));
-		const routes = domainConfig.routes;
-		const fallback = domainConfig.fallback;
-
-		// Split the username part by dot or plus.
-		const username = message.to.split("@")[0].split(/\.|\+/)[0];
 
 		// Check if the username is in the routes.
-		if (username in routes) {
-			for (let forwardingAddr of (Array.isArray(routes[username]) ? routes[username] : [routes[username]])) {
-				await message.forward(forwardingAddr);
-			}
-		} else {
-			// No route for this user, check if there is a fallback.
-			if (fallback) {
-				//TODO: should we also try the global fallback in this case?
-				await message.forward(fallback);
-			} else {
-				console.warn(`Got email for user ${username} of domain ${incomingDomain} but no routes configured for that user and no fallback address available.`);
-				message.setReject("550 5.1.1 User unknown");
-			}
+		if (username in domainConfig.routes) {
+			return handleEmailSend(message, domainConfig.routes[username])
 		}
-	} else {
-		// We got an email for a domain that isn't configured.
-		// Check if there is a fallback address configured.
-		if (domains.includes("fallback")) {
-			console.warn(`Got email for domain ${incomingDomain} but no routes configured. Falling back to ${env.config.get('fallback')}`);
-			await message.forward(env.config.get('fallback'));
-		} else {
-			console.warn(`Got email for domain ${incomingDomain} but no routes configured and no fallback address available.`);
-			message.setReject("550 5.1.1 User unknown");
+		
+		//TODO: should we also try the global fallback in this case?
+		if (domainConfig.fallback) {
+			return await message.forward(domainConfig.fallback);
 		}
+		
+		// Split the username part by dot or plus.
+		const username = message.to.split("@")[0].split(/\.|\+/)[0];
+		console.warn(`Got email for user ${username} of domain ${incomingDomain} but no routes configured for that user and no fallback address available.`);
+		message.setReject("550 5.1.1 User unknown");
+		return;
+	} 
+	
+	if (domains.includes("fallback")) {
+		console.warn(`Got email for domain ${incomingDomain} but no routes configured. Falling back to ${env.config.get('fallback')}`);
+		await message.forward(env.config.get('fallback'));
+		return
 	}
+	
+	console.warn(`Got email for domain ${incomingDomain} but no routes configured and no fallback address available.`);
+	message.setReject("550 5.1.1 User unknown");
+}
+
+async function handleEmailSend(message, forwardingAddr) {
+	return Promise.all(
+		(Array.isArray(forwardingAddr) ? forwardingAddr : [forwardingAddr])
+			   .map(email => message.forward(forwardingAddr)
+	);
 }
 
 export default {
